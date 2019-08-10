a1cb5f8920fbbf21c47aad480e810032ccd332a5
TayloreZhou <38875765+TayloreZhou@users.noreply.github.com>
2019-08-10 20:59:04 +0800

    Update controller.py
    Extend the tags of picture by using synonym retrieval

--- controller.py
+++ controller.py
@@ -16,6 +16,8 @@ app = Flask(__name__)
 application = ClarifaiApp(api_key='c63b5fc84f444eb5b853cddf68ec257b')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER  # 设置文件下载路径
 
+synonymKey='1c119e1ba1b9ef7a'
+requestPara='https://api.jisuapi.com/jinyifanyi/word?appkey='+synonymKey+'&word='
 
 # def allowed_file(filename):  # 通过将文件名分段的方式查询文件格式是否在允许上传格式范围之内
     # return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -38,11 +40,27 @@ def controller():
     for concept in response_data['outputs'][0]['data']['concepts']:
         tag_urls.append(concept['name'])
 
-    tag_string = " ".join(str(i) for i in tag_urls)
+    print(tag_urls)#打表
+    synResult=[]
+    synPara=[]
+    for index in range(len(tag_urls)):
+        synPara.append(requestPara+tag_urls[index])
+        print(synPara[-1])
+        r=requests.get(synPara[-1])
+        result=json.loads(r.text)
+        print(result)#打表
+        if result["status"]!=0 or result["result"]["jin"][0]=="":
+            synResult.append(tag_urls[index])
+        else:
+            synResult.extend(result["result"]["jin"])
+    print(synResult)
+
+    ##tag_urls被我修改为请求的完整url了，所有的结果存在synResult（同义词查询结果）
+    tag_string = " ".join(str(i) for i in synResult)
     print(tag_string)
     j = {"query": {"match": {"content": tag_string}}}
     raw = json.dumps(j)
-    r = requests.post('http://127.0.0.1:9200/poems/poem/_search', data=raw)
+    r = requests.post('http://127.0.0.1:9200/poems/poem/search', data=raw)
 
     result = json.loads(r.text)
     poems = []
@@ -52,11 +70,9 @@ def controller():
                       'dynasty': poem['_source']['dynasty'],
                       'author': poem['_source']['author']})
 
-
-
     os.remove(temp_path)
     print(result)
-    return json.dumps(poems)
+    return json.dumps(tag_string)
 
 
 if __name__ == '__main__':
