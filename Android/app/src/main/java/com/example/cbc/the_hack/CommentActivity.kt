9a909fd9b21a6a692b2d5a9f177cde6334c5f3ab
KHAKhazeus <khazeus@outlook.com>
2019-08-15 15:13:54 +0800

    add things

---
+++ Android/app/src/main/java/com/example/cbc/the_hack/CommentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cbc.the_hack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
